ca5c4b538dd8959a270de4c8b12199c6b1f787f9
Dilip Singh Sisodiya <49243159+dilipsinghsisodiya@users.noreply.github.com>
2024-01-06 18:44:26 +0530

    Update script.js

--- script.js
+++ script.js
@@ -24,26 +24,27 @@ fetch("https://api.github.com/users")
 
 
 // Second Approach Async/Await
-/* async function API(){
-   try {
-       const cards = document.querySelector('.cards');
-        const data = await fetch("https://api.github.com/users");
-        const allData = await data.json();    
-       let myCard = ''
-        await allData.map((val) => { 
-            myCard += `<div class="card">
-        <img src="${val.avatar_url}">
-        <h2 class="username">${val.login}</h2>
-        <a href="${val.html_url}" class="button">
-        <i class="fa fa-github"></i>
-        GitHub
-        </a>
-    </div>`
-        })
-        cards.innerHTML = myCard
-   } catch (error) {
-    console.log('Error: ', error);
-   }
-}
-API()
+/*
+async function API(){
+    try {
+        const cards = document.querySelector('.cards');
+        let allCards = ''
+        const res = await fetch("https://api.github.com/users");
+         const data = await res.json();    
+         await data.map((val) => { 
+            allCards += `<div class="card">
+         <img src="${val.avatar_url}">
+         <h2 class="username">${val.login}</h2>
+         <a href="${val.html_url}" class="button">
+         <i class="fa fa-github"></i>
+         GitHub
+         </a>
+     </div>`
+         })
+         cards.innerHTML = allCards
+    } catch (error) {
+     console.log('Error: ', error);
+    }
+ }
+ API()
 */
