7b836d8d168cdc8b687c50c039ebe4422aba9569
Dilip Singh Sisodiya <49243159+dilipsinghsisodiya@users.noreply.github.com>
2024-01-06 18:37:10 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+// First Approach Promise Chaining
 fetch("https://api.github.com/users")
 .then((res)=>{
     return res.json()
@@ -20,3 +21,29 @@ fetch("https://api.github.com/users")
 .catch((err) => {
     console.log(err);
 })
+
+
+// Second Approach Async/Await
+/* async function API(){
+   try {
+       const cards = document.querySelector('.cards');
+        const data = await fetch("https://api.github.com/users");
+        const allData = await data.json();    
+       let myCard = ''
+        await allData.map((val) => { 
+            myCard += `<div class="card">
+        <img src="${val.avatar_url}">
+        <h2 class="username">${val.login}</h2>
+        <a href="${val.html_url}" class="button">
+        <i class="fa fa-github"></i>
+        GitHub
+        </a>
+    </div>`
+        })
+        cards.innerHTML = myCard
+   } catch (error) {
+    console.log('Error: ', error);
+   }
+}
+API()
+*/
